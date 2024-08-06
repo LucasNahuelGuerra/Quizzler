04e45423974628434eb66a135d4bbdaa6c9916f9
Lucas Nahuel Guerra <lucasnahuelguerra@icloud.com>
2024-08-06 17:22:56 -0300

    Quizzler

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Lucas Nahuel Guerra on 18/07/2024.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
+
+    
